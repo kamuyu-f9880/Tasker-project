31a698f391df51ee8f713267f9ad9c528015c07a
fred <daktarispacetime@gmail.com>
2023-03-26 18:39:07 +0300

    login page

--- tasker-front/src/App.jsx
+++ tasker-front/src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/Login'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
+
+    <>
     <div className="App">
-    
+      <Login />
     </div>
+    
+    
+    </>
   )
 }
 
---
+++ tasker-front/src/pages/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    if (username === 'example' && password === 'password') {
+      alert('Logged in successfully!');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+  );
+}
+
+export default LoginPage;
